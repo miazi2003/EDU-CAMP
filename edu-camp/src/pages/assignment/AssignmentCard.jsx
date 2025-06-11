9c4915f2dbba98cad25d3617454c22dfd03428f1
miazi2003 <yeasinmiazi1997@gmail.com>
2025-06-11 12:12:15 +0600

    assignment Delete function added

--- edu-camp/src/pages/assignment/AssignmentCard.jsx
+++ edu-camp/src/pages/assignment/AssignmentCard.jsx
@@ -5,52 +5,62 @@ import { MdDelete } from "react-icons/md";
 import { CiEdit } from "react-icons/ci";
 import { MdOutlineRemoveRedEye } from "react-icons/md";
 
-const AssignmentCard = ({ assignment }) => {
+
+const AssignmentCard = ({ assignment , handleDelete}) => {
   console.log(assignment);
 
-  const { title, description, marks, difficulty , thumbnailImageURL } = assignment;
+  const { title, description, marks, difficulty, thumbnailImageURL, _id } =
+    assignment;
+
+ 
 
   return (
     <div>
       <div className="card bg-base-100 h-100  shadow-sm flex flex-wrap">
         <figure className="h-42 w-full">
-          <img
-            src={thumbnailImageURL}
-            alt="Shoes"
-          />
+          <img src={thumbnailImageURL} alt="Shoes" />
         </figure>
         <div className="flex">
-            <div className="card-body w-[90%] flex flex-col">
-          <h2 className="fontSec flex  gap-2 text-lg font-bold">
-            <MdOutlineSubtitles /> {title}
-          </h2>
-          <p className="text-gray-500 font text-xs flex gap-2 ">
+          <div className="card-body w-[90%] flex flex-col">
+            <h2 className="fontSec flex  gap-2 text-lg font-bold">
+              <MdOutlineSubtitles /> {title}
+            </h2>
+            <p className="text-gray-500 font text-xs flex gap-2 ">
               {description}
-
-          </p>
-          <p className="flex gap-2 text-gray-500 ">
-            {" "}
-            <span className="text-black">Marks :</span> {marks}
-          </p>
-          <div className="">
-            <p className="flex gap-2  text-gray-500">
-              <span className="text-black">Difficulty :</span>
-              {difficulty}{" "}
             </p>
-          </div>
-          {/* <div className="card-actions justify-end">
+            <p className="flex gap-2 text-gray-500 ">
+              {" "}
+              <span className="text-black">Marks :</span> {marks}
+            </p>
+            <div className="">
+              <p className="flex gap-2  text-gray-500">
+                <span className="text-black">Difficulty :</span>
+                {difficulty}{" "}
+              </p>
+            </div>
+            {/* <div className="card-actions justify-end">
       <div className="badge badge-outline">Fashion</div>
       <div className="badge badge-outline">Products</div>
     </div> */}
-        </div>
+          </div>
 
-        <div className="flex flex-col items-center gap-3 justify-center w-[10%] lg:pr-2  text-right">
-            <button title="Delete" className="text-red-600"><MdDelete /></button>
-            <button title="Edit" className="text-blue-800"><CiEdit /></button>
-            <button title="View Assignment Details" className="text-green-800"><MdOutlineRemoveRedEye /></button>
-        </div>
-
-
+          <div className="flex flex-col items-center gap-3 justify-center w-[10%] lg:pr-2  text-right">
+            <button
+              title="Delete"
+              className="text-red-600"
+              onClick={() => {
+                handleDelete(_id);
+              }}
+            >
+              <MdDelete />
+            </button>
+            <button title="Edit" className="text-blue-800">
+              <CiEdit />
+            </button>
+            <button title="View Assignment Details" className="text-green-800">
+              <MdOutlineRemoveRedEye />
+            </button>
+          </div>
         </div>
       </div>
     </div>
--- edu-camp/src/pages/assignment/Assignments.jsx
+++ edu-camp/src/pages/assignment/Assignments.jsx
@@ -18,12 +18,36 @@ const Assignments = () => {
       });
   }, []);
 
+
+   const handleDelete = (id) => {
+      axios
+        .delete(`http://localhost:3000/createAssignment/${id}`)
+        .then((res) => {
+          console.log(res.data);
+          if(res.data.deletedCount > 0){
+            console.log('deleted')
+            const remainingAssignment = assignments.filter(assignment=>assignment._id !== id)
+            setAssignments(remainingAssignment)
+            
+          }
+        })
+        .catch((err) => {
+          err;
+        });
+    };
+
   return <>
 
-  <div>
+  <div >
+     <h1 className="text-5xl text-center mt-4 text-gray-700 ">
+          All Assignments
+        </h1>
+           <p className="text-lg text-gray-500 mt-2 font text-center">
+          Choose Your Assignment And Grow Your Career Now
+        </p>
     <div className="grid grid-cols-4 gap-8 p-8">
         {
-            assignments.map(assignment=><AssignmentCard assignment={assignment} key={assignment.id}></AssignmentCard>)
+            assignments.map(assignment=><AssignmentCard assignment={assignment} key={assignment.id} handleDelete={handleDelete}></AssignmentCard>)
         }
     </div>
   </div>
